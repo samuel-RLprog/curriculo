98b05ab6ece97c2d7570abad0d35c2b99a90e68d
Samuel Rodrigus <samuelrodrigus@gmail.com>
2026-09-06 07:16:08 -0300

    Update base path in vite.config.ts
    Changed the base path for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/curriculo/', // <-- Altere de './' para '/curriculo/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
