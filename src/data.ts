c71142b9f24bb98629d261560e61efcb301a15fd
Samuel Rodrigus <samuelrodrigus@gmail.com>
2026-09-08 12:01:12 -0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -11,7 +11,7 @@ export const PERSONAL_INFO = {
   phone: '(61) 9 9285-7256',
   phoneRaw: '5561992857256',
   email: 'samuelrodrigus@gmail.com',
-  linkedin: 'www.linkedin.com/in/samuelr',
+  const linkedin = "linkedin.com/in/samuelrl",
   linkedinDisplay: 'linkedin.com/in/samuelr',
   github: 'https://github.com/samuel-RLprog?tab=repositories',
   githubUser: 'samuel-RLprog',
